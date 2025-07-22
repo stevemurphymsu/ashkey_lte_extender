fffb2c4598eca254e5f80f74d185bbd65e78a18d
stevemurphymsu <73205562+stevemurphymsu@users.noreply.github.com>
2025-07-22 15:19:04 -0400

    fixed bad function call

--- custom_components/ashkey-lte/__init__.py
+++ custom_components/ashkey-lte/__init__.py
@@ -30,7 +30,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         "last_reboot": None
     }
 
-    await hass.config_entries.async_forward_entry_setups(hass, entry, "sensor")
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
 
     async def handle_refresh(call):
         await api.authenticate()

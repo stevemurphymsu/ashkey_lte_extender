e82f51fa86eeb962136f928f7ec69d9b778ba746
stevemurphymsu <73205562+stevemurphymsu@users.noreply.github.com>
2025-07-22 15:21:18 -0400

    fixed bad function call

--- custom_components/ashkey-lte/__init__.py
+++ custom_components/ashkey-lte/__init__.py
@@ -31,7 +31,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     }
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, "sensor")
     )
 
     async def handle_refresh(call):
